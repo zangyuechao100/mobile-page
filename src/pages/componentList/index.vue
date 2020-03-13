5ee09de48a98ddc0d0526b16b6167a59f9fac6f9
zangyuechao <zangyuechao@heydayscape.com>
2020-03-13 17:14:00 +0800

    feat：初始化项目

---
+++ src/pages/componentList/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="component-list">
+    <ul class="component-show">
+      <li class="component-item">1</li>
+      <li class="component-item">1</li>
+      <li class="component-item">1</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentList'
+}
+</script>
+
+<style scoped>
+.component-list {
+  box-shadow: 1px 0 0 0 #f2f2f2;
+  height: 100%;
+}
+.component-show {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.component-item {
+  width: 100px;
+  height: 100px;
+}
+
+.component-item:hover {
+  background: #f5fbfb;
+  cursor: pointer;
+}
+</style>
---
+++ src/pages/main/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="main-wrapper">
+    <div class="main-left">
+      <component-list></component-list>
+    </div>
+    <div class="main-center">
+
+    </div>
+    <div class="main-right">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import ComponentList from './../componentList'
+export default {
+  name: 'Main',
+  components: {
+    ComponentList
+  }
+}
+</script>
+
+<style>
+.main-wrapper {
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+}
+.main-left {
+  flex: 0 0 300px;
+  height: 100%;
+}
+
+.main-center {
+  flex: 1 0;
+}
+
+.main-right {
+  flex: 0 0 350px;
+}
+</style>
